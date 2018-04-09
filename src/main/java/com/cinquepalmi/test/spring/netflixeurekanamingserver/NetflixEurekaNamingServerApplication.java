7b07f8360cec465e7b9d8e77f43428e42b262b68
frankid <f_cinquepalmi@yahoo.it>
2018-04-09 23:07:51 +0200

    First commit

---
+++ src/main/java/com/cinquepalmi/test/spring/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.cinquepalmi.test.spring.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+}
